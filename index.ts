3ffc6603fcb52633ca3c049fe967f5221d64364b
nadiawarda <nadiaw99@yahoo.com>
2024-04-17 19:52:33 +0500

    2nd commit

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 #! /usr/bin/env node
-
 import inquirer from "inquirer";
 import chalk from 'chalk';
 
@@ -10,11 +9,9 @@ const answer: {sentence: string;} = await inquirer.prompt([
  	message : "Enter your sentence to count the word: ",
  }
 ]);
-
 const words = answer.sentence.trim().split(" ");
 
 console.log(chalk.blueBright.italic.bold("Your sentence is here: ", words));
-
 console.log(chalk.cyanBright.italic.bold(`Total sentence words counting is:  ${words.length}`));
 console.log(words);
 
