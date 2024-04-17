3625b36399ea3f73ad4dbcbab730321770119e1f
nadiawarda <nadiaw99@yahoo.com>
2024-04-17 20:36:32 +0500

    1st commit

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from 'chalk';
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: ",
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(chalk.blueBright.italic.bold("Your sentence is here: ", words));
+console.log(chalk.cyanBright.italic.bold(`Total sentence words counting is:  ${words.length}`));
+console.log(words);
